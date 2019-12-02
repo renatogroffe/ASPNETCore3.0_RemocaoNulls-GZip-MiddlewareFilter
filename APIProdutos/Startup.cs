f53e7d4e823c16dd88f03cb57be1ed75221a2289
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-02 03:07:26 -0300

    ASP.NET Core 3.0 + Remoção Nulls e Propriedades Read-Only + Compressão GZip via MiddlewareFilter
    ASP.NET Core 3.0 + Remoção Nulls e Propriedades Read-Only + Compressão GZip via MiddlewareFilter

---
+++ APIProdutos/Startup.cs
@@ -0,0 +1,63 @@
+using System.IO.Compression;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace APIProdutos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configura o modo de compressão
+            services.Configure<GzipCompressionProviderOptions>(
+                options => options.Level = CompressionLevel.Optimal);
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
+
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    var serializerOptions = options.JsonSerializerOptions;
+                    serializerOptions.IgnoreNullValues = true;
+                    serializerOptions.IgnoreReadOnlyProperties = true;
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Ativa a compressão
+            app.UseResponseCompression();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
